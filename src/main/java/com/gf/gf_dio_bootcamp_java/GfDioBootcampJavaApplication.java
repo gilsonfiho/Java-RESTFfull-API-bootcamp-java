2e070cc188fe2f08d2a1248d6251a1c684f3f7be
Gilson Almeida <gilson405719@hotmail.com>
2025-09-19 09:35:33 -0300

    Initial commit

---
+++ src/main/java/com/gf/gf_dio_bootcamp_java/GfDioBootcampJavaApplication.java
@@ -0,0 +1,13 @@
+package com.gf.gf_dio_bootcamp_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfDioBootcampJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfDioBootcampJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gf/gf_dio_bootcamp_java/GfDioBootcampJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gf.gf_dio_bootcamp_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GfDioBootcampJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
